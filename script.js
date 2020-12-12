94783b577b4633064399bbc20e521ca9f5e946fa
Max Einhorn <maxeinhorn@gmail.com>
2020-12-12 14:20:07 -0800

    Added k6 for load testing

---
+++ script.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 10,
+  stages: [
+    {duration: '5s', target: 1200},
+    {duration: '5s', target: 8000},
+    {duration: '5s', target: 0},
+  ]
+};
+
+export default function () {
+  var id = Math.ceil((Math.random() * 10000000))
+  let res = http.get(`http://localhost:3000/${id}`);
+  check(res, {' status was 200': (r) => r.status === 200 });
+  sleep(1);
+}
+
+// k6 run script.js
